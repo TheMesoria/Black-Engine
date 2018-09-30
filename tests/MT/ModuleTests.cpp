1058fd838e3fffb4c3359fbc819f268a43dadbf6
Black <freedomandjustit@gmail.com>
2018-09-30 14:16:50 +0200

    Save of working project

---
+++ tests/MT/ModuleTests.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Wojciech Ormaniec on 30.09.18.
+// Copyright (c) 2018 Industry of Black. All rights reserved.
+//
+#include <gtest/gtest.h>
+
+int StartGoogleTest(int argc, char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+
+	return RUN_ALL_TESTS();
+}
