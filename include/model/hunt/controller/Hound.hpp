8a5864ba3f6a8c95ecf22a96149a37c1aa129128
Black <freedomandjustit@gmail.com>
2018-10-17 19:40:55 +0200

    Sem.

--- include/model/hunt/controller/Hound.hpp
+++ include/model/hunt/controller/Hound.hpp
@@ -2,14 +2,19 @@
 // Created by black on 16.10.18.
 //
 
+#include "model/hunt/HuntObject.hpp"
+
 #ifndef BLACK_ENGINE_HOUND_HPP
 #define BLACK_ENGINE_HOUND_HPP
 
-
-class Hound
+namespace Model
 {
+	class Hound
+			: public HuntObject
+	{
 
-};
+	};
+}
 
 
 #endif //BLACK_ENGINE_HOUND_HPP
