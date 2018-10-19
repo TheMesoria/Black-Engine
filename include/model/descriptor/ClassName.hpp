c80c776cdd9a48c0c9887f269546d7cf85f496a8
Black <freedomandjustit@gmail.com>
2018-10-19 17:12:38 +0200

    Works

---
+++ include/model/descriptor/ClassName.hpp
@@ -0,0 +1,9 @@
+//
+// Created by Wojciech Ormaniec on 19.10.18.
+// Copyright (c) 2018 Industry of Black. All rights reserved.
+//
+
+#ifndef BLACK_ENGINE_CLASSNAME_HPP
+#define BLACK_ENGINE_CLASSNAME_HPP
+
+#endif //BLACK_ENGINE_CLASSNAME_HPP
