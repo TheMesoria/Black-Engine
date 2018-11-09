da0239c2d77f20fe701ebfa5050104158b484e21
Black <freedomandjustit@gmail.com>
2018-11-09 16:40:01 +0100

    Hot fix for failing build on TC

---
+++ runnable/main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Wojciech Ormaniec on 07.11.18.
+// Copyright (c) 2018 Industry of Black. All rights reserved.
+//
+
+
+
+#include <engine/Huntsman.hpp>
+
+int main( int argc, char** args )
+{
+	huntsman::Huntsman huntsman;
+	huntsman.start();
+}
