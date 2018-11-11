b51355cea60f12a9da35924e650b50448b9e10bf
Black <freedomandjustit@gmail.com>
2018-11-11 19:42:02 +0100

    Hunting ground start

---
+++ huntsman/include/engine/view/HuntingGround.hpp
@@ -0,0 +1,15 @@
+//
+// Created by Wojciech Ormaniec on 11.11.18.
+// Copyright (c) 2018 Industry of Black. All rights reserved.
+//
+#pragma once
+
+
+namespace huntsman
+{
+	class HuntingGround
+	{
+
+	};
+}
+
