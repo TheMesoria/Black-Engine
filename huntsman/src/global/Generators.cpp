72d1b70db1f6410f4f24b00024b276bb0ef3796c
Black <freedomandjustit@gmail.com>
2018-12-01 20:20:18 +0100

    Fix  to

--- huntsman/src/global/Generators.cpp
+++ huntsman/src/global/Generators.cpp
@@ -7,36 +7,36 @@
 
 namespace
 {
-	std::string GeneratePartialId( uint8_t size )
-	{
-		static const char alphabet[] =
-								  "abcdefghijklmnopqrstuvwxyz"
-								  "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-								  "0123456789";
+std::string GeneratePartialId(uint8_t size)
+{
+    constexpr char alphabet[] =
+                       "abcdefghijklmnopqrstuvwxyz"
+                       "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                       "0123456789";
 
-		std::random_device              rd;
-		std::default_random_engine      rng( rd());
-		std::uniform_int_distribution<> dist( 0, sizeof( alphabet ) / sizeof( *alphabet ) - 2 );
-		std::string str;
+    std::random_device              rd;
+    std::default_random_engine      rng(rd());
+    std::uniform_int_distribution<> dist(0, sizeof(alphabet) / sizeof(*alphabet) - 2);
+    std::string                     str;
 
-		str.reserve( size );
-		std::generate_n( std::back_inserter( str ), size,
-						 [ & ]() { return alphabet[ dist( rng ) ]; } );
+    str.reserve(size);
+    std::generate_n(std::back_inserter(str), size, [&]()
+    { return alphabet[dist(rng)]; });
 
-		return str;
-	}
+    return str;
+}
 }
 
 namespace Global::Default::Functions
 {
-	std::string GenerateId( uint8_t size, uint8_t partialSize )
-	{
-		std::string returnId = "";
-		for(auto i = 0u; i < size; i++)
-		{
-			returnId+=GeneratePartialId(partialSize)+"-";
-		}
-		returnId.pop_back();
-		return returnId;
-	}
+std::string GenerateId(uint8_t size, uint8_t partialSize)
+{
+    std::string returnId = "";
+    for (auto   i        = 0u; i < size; i++)
+    {
+        returnId += GeneratePartialId(partialSize) + "-";
+    }
+    returnId.pop_back();
+    return returnId;
+}
 }
